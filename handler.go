a8eca72c5788e4cfa740dba126b36a75faa3908f
maruware <me@maruware.com>
2022-02-05 14:59:37 +0900

    Refactor

--- handler.go
+++ handler.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 )
 
@@ -17,26 +18,41 @@ type RequestLog struct {
 	Method string `json:"method"`
 	Path   string `json:"path"`
 	IsTLS  bool   `json:"is_tls"`
+	Body   string `json:"body"`
 }
 
 func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if err := h.handle(w, r); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		logger.Errorf(err.Error())
+		fmt.Fprintf(w, err.Error())
+	}
+}
+
+func (h *Handler) handle(w http.ResponseWriter, r *http.Request) error {
+	defer r.Body.Close()
+
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		return err
+	}
+
 	l := RequestLog{
 		Method: r.Method,
 		Path:   r.URL.Path,
 		IsTLS:  r.TLS != nil,
+		Body:   string(body),
 	}
 
-	b := bytes.NewBuffer(nil)
-	enc := json.NewEncoder(b)
+	logBuf := bytes.NewBuffer(nil)
+	enc := json.NewEncoder(logBuf)
 
 	if err := enc.Encode(l); err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		logger.Errorf(err.Error())
-		fmt.Fprintf(w, "err.Error()")
-		return
+		return err
 	}
 
-	logger.Printf(b.String())
+	logger.Printf(logBuf.String())
 
 	fmt.Fprintf(w, "OK")
+	return nil
 }
