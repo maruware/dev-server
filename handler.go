340f8d0f12f93b55e8f9ea2ccd94e41ed9c62d3a
maruware <me@maruware.com>
2022-02-06 10:31:26 +0900

    Change json log format

--- handler.go
+++ handler.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"time"
 
 	"github.com/fatih/color"
 )
@@ -18,7 +19,8 @@ func NewHandler(format string) *Handler {
 	return &Handler{format: format}
 }
 
-type RequestLog struct {
+type RequestSummary struct {
+	Time     time.Time   `json:"time"`
 	Method   string      `json:"method"`
 	Path     string      `json:"path"`
 	IsTLS    bool        `json:"is_tls"`
@@ -43,7 +45,8 @@ func (h *Handler) handle(w http.ResponseWriter, r *http.Request) error {
 		return err
 	}
 
-	l := RequestLog{
+	l := RequestSummary{
+		Time:     time.Now(),
 		Method:   r.Method,
 		Path:     r.URL.Path,
 		IsTLS:    r.TLS != nil,
@@ -58,7 +61,7 @@ func (h *Handler) handle(w http.ResponseWriter, r *http.Request) error {
 		if err != nil {
 			return err
 		}
-		logger.Printf(s)
+		fmt.Println(s)
 		break
 	case "simple":
 		s := l.formatSimple()
@@ -74,15 +77,15 @@ func (h *Handler) handle(w http.ResponseWriter, r *http.Request) error {
 	return nil
 }
 
-func (rl RequestLog) formatSimple() string {
+func (rl RequestSummary) formatSimple() string {
 	return fmt.Sprintf("%s %s %s %s", rl.Method, rl.Path, rl.Protocol, rl.Body)
 }
 
-func (rl RequestLog) formatSimpleColor() string {
+func (rl RequestSummary) formatSimpleColor() string {
 	return fmt.Sprintf("%s %s %s %s", color.GreenString(rl.Method), rl.Path, rl.Protocol, rl.Body)
 }
 
-func (rl RequestLog) formatJSON() (string, error) {
+func (rl RequestSummary) formatJSON() (string, error) {
 	logBuf := bytes.NewBuffer(nil)
 	enc := json.NewEncoder(logBuf)
 
